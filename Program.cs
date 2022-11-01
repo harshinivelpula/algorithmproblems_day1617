2207971343598d83cccf967e40c8224ce5292be2
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-01 15:19:28 +0530

    prime number

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace algorithmproblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to primenumber problem");
+
+            primenumber.prime();
+        }
+    }
+}
