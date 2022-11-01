b288759fb04c164f817664e5281c7f06cb00757a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-01 15:20:20 +0530

    find primenumber in range

---
+++ primenumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmproblems
+{
+    internal class primenumber
+    {
+        public static void prime()
+        {
+            for (int number = 1; number <= 1000; number++)
+            {
+                int count = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        count++;
+                    }
+
+
+
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine("prime number:" + number);
+                }
+
+            }
+        }
+    }
+}
