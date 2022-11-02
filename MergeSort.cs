be1354be43a9230596153b7f16c7695ead4d7cba
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-02 19:38:32 +0530

    merge

---
+++ MergeSort.cs
@@ -0,0 +1,27 @@
+﻿namespace algorithmproblems
+{
+    public class MergeSort
+	{
+		public static void Display()
+		{
+			MergeSort mergeSort = new MergeSort();
+			String[] arr = { "charu", "chocky", "haswika", "haswik", "kavitha", "ravi" };
+			int start = 0, end = arr.Length - 1;
+			mergeSort.mergeSort(arr, start, end);
+            MergeSort.printStringArr(arr);
+
+		}
+
+        private void mergeSort(string[] arr, int start, int end)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void printStringArr(string[] arr)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
