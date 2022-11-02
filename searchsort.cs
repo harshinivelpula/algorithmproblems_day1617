54ecc2586c5ed791c1b7b41be4c1a5678fa42c34
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-02 20:48:44 +0530

    search sort

---
+++ searchsort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algorithmproblems
+{
+    internal class searchsort
+    {
+        public static void Display()
+        {
+            static void selectionSort(int[] array)
+            {
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    int min = array[i];
+                    int minId = i;
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[j] < min)
+                        {
+                            min = array[j];
+                            minId = j;
+                        }
+                    }
+                    int temp = array[i];
+                    array[i] = min;
+                    array[minId] = temp;
+                }
+
+            }
+
+
+        }
+    }
+}
